30d961019331d65774632b51728d78296ef3065a
VarentsovaMV <varentsovamv@gmail.com>
2023-03-28 14:54:39 +0300

    Создание и вывод на экран строчного массива по количеству размера(n) и текста под каждый элемент от пользователя

--- Programma/Program.cs
+++ Programma/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Описываем метод создания строчного массива, принимающего n - количество элементов массива
+// пользователь вводит размер массива (количество элементов)
+// пользователь вводит текст строки под каждый элемнет массива
+string [] CreateArray(int n)
+{
+    string [] str = new string[n];
+    for (int i = 0; i < n; i++)
+    {
+        Console.WriteLine("Введите элемент: "+ i);
+        string text = Convert.ToString(Console.ReadLine());
+        str[i] = text;
+    }
+    return str;
+}
+// Описываем метод вывода на экран заданного пользователем строчного массива
+void ShowArray(string [] str)
+{
+    for (int i = 0; i < str.Length; i++)
+    {
+        Console.Write('"' + str[i] + '"' + "," + " ");
+    }        
+    Console.WriteLine();
+}
+
+Console.WriteLine("Введите количество n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string[] str = CreateArray(n);
+ShowArray (str);
